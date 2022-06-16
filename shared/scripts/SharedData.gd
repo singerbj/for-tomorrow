8f5666c03d6b1b1a137c2b58784020acffed634d
Benjamin Singer <benjmainjsinger@gmail.com>
2022-06-16 16:27:52 -0500

    minor interpolation offset value change

--- shared/scripts/SharedData.gd
+++ shared/scripts/SharedData.gd
@@ -3,5 +3,5 @@ extends Node
 const SENS_MULTIPLIER : float = 0.03
 
 # How many seconds "behind the received information" the client is
-const INTERPOLATION_OFFSET : int = 250	# 100ms
+const INTERPOLATION_OFFSET : int = 100	# 100ms
 const GRAVITY : float = -40.0
